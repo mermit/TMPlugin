47e5f930a7147e746f241cd8d573e68e6d94a636
Tim <Tim>
2017-01-28 15:47:46 +0100

    Initial commit

---
+++ src/Netherlands/tpg01ANDmer_mit/TMPlugin/Main.java
@@ -0,0 +1,16 @@
+package Netherlands.tpg01ANDmer_mit.TMPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        //Fired when the server enables the plugin
+    }
+
+    @Override
+    public void onDisable(){
+        //Fired when the server stops and disables all plugins
+    }
+}
