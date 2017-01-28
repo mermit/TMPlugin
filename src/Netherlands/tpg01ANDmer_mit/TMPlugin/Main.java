c491aa51847b42b666702ccb88968da7a40f2a91
BuildTools <unconfigured@null.spigotmc.org>
2017-01-28 17:33:14 +0100

    Added console logger message "Enabling TMP"

--- src/Netherlands/tpg01ANDmer_mit/TMPlugin/Main.java
+++ src/Netherlands/tpg01ANDmer_mit/TMPlugin/Main.java
@@ -1,5 +1,6 @@
 package Netherlands.tpg01ANDmer_mit.TMPlugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -7,6 +8,7 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable(){
         //Fired when the server enables the plugin
+    	System.out.println("Enabling TMP");
     }
 
     @Override
